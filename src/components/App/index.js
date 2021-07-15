3ef1a1082100624901f20ac0886f7aea575a4e94
RomainLPromoLyraJS <romain75l@gmail.com>
2021-07-15 19:00:36 +0200

    Application works

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,57 @@
+import React, { useState } from "react";
+import Cards from "react-credit-cards";
 
-function App() {
+// == CSS Card Component
+import "react-credit-cards/es/styles-compiled.css";
+
+
+const App = () => {
+
+const [isNumber, setNumber] = useState("");
+const [isName, setName] = useState("");
+const [isExpiry, setExpiry] = useState("");
+const [isCvc, setCvc] = useState("");
+const [isFocus, setFocus] = useState("");
+
   return (
     <div className="App">
-      toto
+      <Cards 
+      number={isNumber} 
+      name={isName} 
+      expiry={isExpiry}
+      cvc={isCvc} 
+      focused={isFocus} 
+      />
+      <form action="">
+        <input 
+        type="tel" 
+        name="number" 
+        placeholder="Card Number" 
+        value={isNumber} 
+        onChange={event => setNumber(event.target.value)}
+        onFocus={event => setFocus(event.target.name)} />
+        <input 
+        type="text" 
+        name="name" 
+        placeholder="Name" 
+        value={isName} 
+        onChange={event => setName(event.target.value)}
+        onFocus={event => setFocus(event.target.name)} />
+        <input 
+        type="tel" 
+        name="expiry" 
+        placeholder="MM/YY Expiry" 
+        value={isExpiry} 
+        onChange={event => setExpiry(event.target.value)}
+        onFocus={event => setFocus(event.target.name)} />
+        <input 
+        type="tel" 
+        name="cvc" 
+        placeholder="CVC" 
+        value={isCvc} 
+        onChange={event => setCvc(event.target.value)}
+        onFocus={event => setFocus(event.target.name)} />
+      </form>
     </div>
   );
 }
